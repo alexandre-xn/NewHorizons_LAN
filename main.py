e47e1c6e114e6728b717d2a7aa980a967e2b2760
Alexandre <alexandrefournier13@gmail.com>
2020-04-14 12:30:14 +0200

    Some evolution in researching UDP packets payload

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ VERBOSE_MODE = sys.argv.pop()
 
 
 def fetch_hosts_from_server(server_name, portnum):
+    hosts = []
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
 
-    if any(c.isalpha() for c in server_name):  # Handle direct IPs without server names
+    if any(c.isalpha() for c in server_name):  # Handle direct IPs without server names otherwise
         ipaddr = socket.gethostbyname(server_name)
     else:
         ipaddr = server_name
@@ -20,41 +21,43 @@ def fetch_hosts_from_server(server_name, portnum):
     s.connect((ipaddr, portnum))
     s.settimeout(2)
 
-    retry_times = 3
-    req_index = 0
-
-    data = ""
-    hosts_found = False
+    retry_times = 5
 
     req = server_reqs[0]
     s.send(bytes.fromhex(req))
 
     try:
-        while retry_times != 0 and not hosts_found:
-            data = s.recv(1024)
+        while retry_times != 0:
+            data = s.recv(2048)
             if VERBOSE_MODE == "debug":
                 print(">>data run " + str(retry_times))
                 print(data)
-            data = data.decode('ascii', 'ignore').replace('\n', '')
-            
-            hosts_found = "12345678" in data
-            retry_times -= 1
-            req_index += 1
 
-        if not hosts_found:
-            data = "No hosts available"
+            hosts_found = "12345678" in data.decode('utf-8', 'ignore')
+
+            if hosts_found and data not in hosts:
+                data = data.decode('utf-8', 'ignore').replace('\n', '')
+                # case russian name : print(data.decode('utf_16', 'ignore').replace('\n', ''))
+                print(data)
+                hosts.append(data)
+
+            retry_times -= 1
 
     except socket.timeout:
-        data = "Timed out"
+        print("Response : Timed out")
 
-    print(" Response : " + data)
+    return hosts
 
 
-fetch_hosts_from_server('switch.lan-play.com', 11452)
-fetch_hosts_from_server('lanplay.reboot.ms', 11451)
-fetch_hosts_from_server('open.t0g3pii.tk', 11451)
-fetch_hosts_from_server('frog-skins.com', 11451)
-fetch_hosts_from_server('switch.jayseateam.nl', 11451)
-fetch_hosts_from_server('chasehall.net', 11451)
-fetch_hosts_from_server('nook-inc.net', 11451)
-fetch_hosts_from_server('81.98.26.94', 11451)
+def fetch_all():
+    print(fetch_hosts_from_server('switch.lan-play.com', 11452))
+    print(fetch_hosts_from_server('lanplay.reboot.ms', 11451))
+    print(fetch_hosts_from_server('open.t0g3pii.tk', 11451))
+    print(fetch_hosts_from_server('frog-skins.com', 11451))
+    print(fetch_hosts_from_server('switch.jayseateam.nl', 11451))
+    print(fetch_hosts_from_server('chasehall.net', 11451))
+    print(fetch_hosts_from_server('nook-inc.net', 11451))
+    print(fetch_hosts_from_server('81.98.26.94', 11451))
+
+
+fetch_all()
