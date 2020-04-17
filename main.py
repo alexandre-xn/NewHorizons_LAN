2efabf677c35b1f25a27b344a3fcfe0c72b4b00a
Alexandre <alexandrefournier13@gmail.com>
2020-04-17 17:50:16 +0200

    End of initial script dev

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import socket
 import sys
+from enum import Enum
+
+
+class PayloadPart(Enum):
+    SPACE_1 = 0,
+    SPACE_2 = 1,
+    LETTER = 2
+
 
 server_reqs = ['0145000028032500004011eb370a0d784f0a0dffff2cbc2cbc0014d4bf001445110000000000000000',
                '0145000028032600004011eb360a0d784f0a0dffff2cbc2cbc0014d4bf001445110000000000000000',
@@ -29,24 +37,81 @@ def fetch_hosts_from_server(server_name, portnum):
     try:
         while retry_times != 0:
             data = s.recv(2048)
-            if VERBOSE_MODE == "debug":
-                print(">>data run " + str(retry_times))
-                print(data)
+            data = data.decode('utf-8', 'ignore')
 
-            hosts_found = "12345678" in data.decode('utf-8', 'ignore')
-
-            if hosts_found and data not in hosts:
-                data = data.decode('utf-8', 'ignore').replace('\n', '')
+            if "12345678" in data:
+                udp_payload = data.replace('\n', '')
                 # case russian name : print(data.decode('utf_16', 'ignore').replace('\n', ''))
-                print(data)
-                hosts.append(data)
+                if VERBOSE_MODE == "debug":
+                    print(udp_payload)
+
+                player_count = convert_raw_int_to_int(udp_payload[str.index(udp_payload, '>') - 2])
+                player_list = []
+                for i in range(0, player_count):
+                    temp_player, udp_payload = extract_next_string_from_udp_payload(udp_payload)
+                    player_list.append(temp_player)
+                island_name, udp_payload = extract_next_string_from_udp_payload(udp_payload)
+                if len(island_name) == 1:
+                    if VERBOSE_MODE == "debug":
+                        print("Missing player names")
+                    if len(player_list[-1]) != 1:
+                        island_name = player_list[-1]
+                    else:
+                        island_name = "???"
+                        player_count = 1
+
+                hosts.append((island_name, player_list, player_count))
+            else:
+                if VERBOSE_MODE == "debug":
+                    print("Data received but no hosts")
 
             retry_times -= 1
 
     except socket.timeout:
-        print("Response : Timed out")
+        print("Response : Timed out (no hosts ?)")
 
     return hosts
+
+
+def extract_next_string_from_udp_payload(payload):
+    payload = payload[::-1]
+    d = payload[0]
+    str_index = 0
+    string = ""
+    while ord(d) <= 32:
+        str_index += 1
+        d = payload[str_index]
+    next_byte = PayloadPart.LETTER
+    next_byte_correct = True
+    while next_byte_correct:
+        string += d
+        str_index += 1
+        d = payload[str_index]
+        if next_byte == PayloadPart.LETTER:
+            next_byte_correct = ord(d) == 0
+            next_byte = PayloadPart.SPACE_1
+        elif next_byte == PayloadPart.SPACE_1:
+            next_byte_correct = ord(d) == 0
+            next_byte = PayloadPart.SPACE_2
+        else:
+            next_byte_correct = 32 <= ord(d) <= 126
+            next_byte = PayloadPart.LETTER
+    payload = payload[::-1]
+    return string[::-1].replace('\x00', ''), payload[0:len(payload) - str_index - 3]
+
+
+def convert_raw_int_to_int(
+        raw_int):  # Converts the Python raw string to a proper int for the player count, i've not found a cleaner way
+    switcher = {
+        '': 1,
+        '': 2,
+        '': 3,
+        '': 4,
+        '': 5,
+        '': 6,
+        '': 7
+    }
+    return switcher.get(raw_int, 1)
 
 
 def fetch_all():
@@ -54,10 +119,13 @@ def fetch_all():
     print(fetch_hosts_from_server('lanplay.reboot.ms', 11451))
     print(fetch_hosts_from_server('open.t0g3pii.tk', 11451))
     print(fetch_hosts_from_server('frog-skins.com', 11451))
-    print(fetch_hosts_from_server('switch.jayseateam.nl', 11451))
     print(fetch_hosts_from_server('chasehall.net', 11451))
     print(fetch_hosts_from_server('nook-inc.net', 11451))
     print(fetch_hosts_from_server('81.98.26.94', 11451))
+    print(fetch_hosts_from_server('lanplay2.reboot.ms', 11451))
+    print(fetch_hosts_from_server('joinsg.net', 11451))
+    print("------------")
+    fetch_all()
 
 
 fetch_all()
